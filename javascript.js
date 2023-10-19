e130d75b6e01b253ebf0abce942d83892d624398
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2023-10-19 21:27:05 +0300

    .

--- javascript.js
+++ javascript.js
@@ -1,6 +1,39 @@
-let buttonGrid = document.querySelector(`.buttons`)
-for(let i = 1; i < 17; i++){
-    let button = document.createElement(`div`);
-    button.classList.add(`button${i}`);
-    buttonGrid.appendChild(button);
-}
+let num1 = document.querySelector(`.button13`);
+num1.innerHTML = `1`;
+let num2 = document.querySelector(`.button14`);
+num2.innerHTML = `2`;
+let num3 = document.querySelector(`.button15`);
+num3.innerHTML = `3`;
+let num4 = document.querySelector(`.button9`);
+num4.innerHTML = `4`;
+let num5 = document.querySelector(`.button10`);
+num5.innerHTML = `5`;
+let num6 = document.querySelector(`.button11`);
+num6.innerHTML = `6`;
+let num7 = document.querySelector(`.button5`);
+num7.innerHTML = `7`;
+let num8 = document.querySelector(`.button6`);
+num8.innerHTML = `8`;
+let num9 = document.querySelector(`.button7`);
+num9.innerHTML = `9`;
+let numX = document.querySelector(`.button1`);
+numX.innerHTML = `x`
+let numDiv = document.querySelector(`.button2`);
+numDiv.innerHTML = `÷`;
+let numPlus = document.querySelector(`.button3`);
+numPlus.innerHTML = `+`;
+let numMinus = document.querySelector(`.button4`);
+numMinus.innerHTML = `-`;
+let numCF = document.querySelector(`.button8`);
+numCF.innerHTML = `CF`;
+let numDelete = document.querySelector(`.button12`);
+numDelete.innerHTML = `⌫`;
+let numEq = document.querySelector(`.button19`);
+numEq.innerHTML = `=`;
+let num0 = document.querySelector(`.button17`);
+num0.innerHTML = `0`;
+let numDot = document.querySelector(`.button18`);
+numDot.innerHTML = `.`;
+let numPlusMinus = document.querySelector(`.button16`);
+numPlusMinus.innerHTML = `+/-`
+
