e27bd9f574a41270eb199d80aff37dfb4f28ac4b
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2023-10-20 10:52:21 +0300

    .

--- javascript.js
+++ javascript.js
@@ -1,41 +1,39 @@
-let num1 = document.querySelector(`.button13`);
-num1.innerHTML = `1`;
-let num2 = document.querySelector(`.button14`);
-num2.innerHTML = `2`;
-let num3 = document.querySelector(`.button15`);
-num3.innerHTML = `3`;
-let num4 = document.querySelector(`.button9`);
-num4.innerHTML = `4`;
-let num5 = document.querySelector(`.button10`);
-num5.innerHTML = `5`;
-let num6 = document.querySelector(`.button11`);
-num6.innerHTML = `6`;
-let num7 = document.querySelector(`.button5`);
-num7.innerHTML = `7`;
-let num8 = document.querySelector(`.button6`);
-num8.innerHTML = `8`;
-let num9 = document.querySelector(`.button7`);
-num9.innerHTML = `9`;
-let numX = document.querySelector(`.button1`);
-numX.innerHTML = `x`
-let numDiv = document.querySelector(`.button2`);
-numDiv.innerHTML = `÷`;
-let numPlus = document.querySelector(`.button3`);
-numPlus.innerHTML = `+`;
-let numMinus = document.querySelector(`.button4`);
-numMinus.innerHTML = `-`;
-let numCF = document.querySelector(`.button8`);
-numCF.innerHTML = `CF`;
-let numDelete = document.querySelector(`.button12`);
-numDelete.innerHTML = `⌫`;
-let numEq = document.querySelector(`.button19`);
-numEq.innerHTML = `=`;
-let num0 = document.querySelector(`.button17`);
-num0.innerHTML = `0`;
-let numDot = document.querySelector(`.button18`);
-numDot.innerHTML = `.`;
-let numPlusMinus = document.querySelector(`.button16`);
-numPlusMinus.innerHTML = `+/-`
+let input = document.querySelector(`.window`);
+let body = document.querySelector(`body`);
+body.appendChild(input)
+let buttonLayout = document.createElement(`div`);
+buttonLayout.classList.add(`buttonlayout`);
+body.appendChild(buttonLayout);
 
+let calculation = [];
+
+let enter = document.createElement(`button`);
+enter.classList.add(`enter`);
+buttonLayout.appendChild(enter);
+enter.innerHTML = `enter`;
+enter.addEventListener(`click`, function(){
+    console.log(calculation)
+})
+
+let del = document.createElement(`button`);
+del.classList.add(`delete`);
+del.innerHTML = `delete`;
+buttonLayout.appendChild(del)
+enter.addEventListener(`click`, function(){
+    input.innerHTML = ``;
+    calculation = [];
+})
+
+
+for (let i = 0; i < 10; i++){
+    let number = document.createElement(`button`)
+    number.classList.add(`number${i}`)
+    number.innerHTML = `${i}`;
+    number.addEventListener(`click`, function(){
+        input.innerHTML += `${i}`;
+        calculation.push(i)
+    })
+    buttonLayout.appendChild(number);
+}
 
 
